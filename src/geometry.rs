5d71343c1ed7cc3ffe54bfc8cecfa2b3797cbf3b
Will Dady <willdady@gmail.com>
2022-08-12 23:12:17 +1000

    feat: added From traits to Point struct

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,12 +1,12 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Point {
     pub x: f64,
-    pub y: f64
+    pub y: f64,
 }
 
 impl Point {
     pub fn new(x: f64, y: f64) -> Point {
-        Point{ x, y }
+        Point { x, y }
     }
 
     pub fn dist(&self, point: &Point) -> f64 {
@@ -17,5 +17,17 @@ impl Point {
         let delta_x = point.x - self.x;
         let delta_y = point.y - self.y;
         delta_y.atan2(delta_x)
+    }
+}
+
+impl From<(u32, u32)> for Point {
+    fn from(t: (u32, u32)) -> Self {
+        Point::new(t.0.into(), t.1.into())
+    }
+}
+
+impl From<(f64, f64)> for Point {
+    fn from(t: (f64, f64)) -> Self {
+        Point::new(t.0, t.1)
     }
 }
