5ee76d90e290c176e0fd6cd9a8d69129294555e8
Will Dady <willdady@gmail.com>
2022-08-12 23:14:12 +1000

    feat: moved swirlr code to it's own module

--- src/main.rs
+++ src/main.rs
@@ -1,166 +1,75 @@
-extern crate image;
 extern crate clap;
+extern crate image;
 
 mod geometry;
 mod svg;
+mod swirlr;
 
-use image::*;
-use clap::{Arg, App};
-use geometry::Point;
+use clap::Parser;
+use swirlr::{MaxRadius, Swirlr};
 
-fn swirl(source: &mut RgbImage) -> (f64, Vec<Point>) {
-    // Output dimension
-    let size = 500.0;
-    // Centre-crop image to a square and resize to `size`
-    let mut im: RgbImage;
-    let (width, height) = source.dimensions();
-    im = if width == height {
-        imageops::crop(source, 0, 0, width, height).to_image()
-    } else if width < height {
-        imageops::crop(source, 0, ((height as f64 - width as f64) * 0.5).floor() as u32, width, width).to_image()
-    } else {
-        imageops::crop(source, ((width as f64 - height as f64) * 0.5).floor() as u32, 0, height, height).to_image()
-    };
-    im = imageops::resize(&im, size as u32, size as u32, imageops::FilterType::Nearest);
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Path to source image (jpeg or png)
+    #[clap(value_parser)]
+    source: String,
 
-    // Keep the spiral within a 5px gutter
-    let max_radius = (size * 0.5) - 5.0;
+    /// Whether spiral should extend past the edge of the canvas
+    #[clap(default_value_t = MaxRadius::Overflow, short, long, arg_enum, value_parser)]
+    max_radius: MaxRadius,
 
-    let origin_x = size * 0.5;
-    let origin_y = size * 0.5;
-    let mut r;
+    /// Spiral color
+    #[clap(default_value_t = String::from("#000"), short, long, value_parser)]
+    color: String,
 
-    let mut theta: f64 = 0.0;
+    /// Background color
+    #[clap(short, long, value_parser)]
+    bg_color: Option<String>,
 
-    let a = 0.0;  // The starting radius
-    let b = 1.2;  // The growth rate of the spiral through each iteration of the loop
-    let sample_length = 7.0;
+    /// Rate at which spiral moves away from origin
+    #[clap(default_value_t = 1.2, short, long, value_parser)]
+    growth_rate: f64,
 
-    let mut inner = vec!();
-    let mut outer = vec!();
+    /// X coordinate of the center of the spiral
+    #[clap(default_value_t = 250.0, short = 'x', long, value_parser)]
+    origin_x: f64,
 
-    // while theta < max_angle {
-    loop {
-        theta += 0.003;
-        r = a + b * theta;
-        if r >= max_radius {
-            break;
-        }
-        // The current point on the spiral
-        let p0 = Point::new(
-            origin_x + r * theta.cos(),
-            origin_y + r * theta.sin()
-        );
-        // We generate two points centered around p0 in the direction of theta,
-        // one towards the center of the spiral, the other away from.
-        let p1 = Point::new(
-            p0.x - (sample_length * 0.5) * theta.cos(),
-            p0.y - (sample_length * 0.5) * theta.sin()
-        );
-        let p2 = Point::new(
-            p0.x + (sample_length * 0.5) * theta.cos(),
-            p0.y + (sample_length * 0.5) * theta.sin()
-        );
-        // Get the average rgb between our two points
-        let average_rgb = get_average_rgb_between_points(&im, &p1, &p2);
-        let luma = average_rgb.to_luma();
-        // Convert average rgb into luma and then normalise the luma to a value
-        // between 0 and sample_length
-        let mut length = ((255.0 - luma.data[0] as f64) / 255.0) * sample_length;
-        // Make sure length is not less than 1, purely for asthetic reasons
-        if length < 1.0 {
-            length = 1.0;
-        }
-        let p1 = Point::new(
-            p0.x - (length * 0.5) * theta.cos(),
-            p0.y - (length * 0.5) * theta.sin()
-        );
-        let p2 = Point::new(
-            p0.x + (length * 0.5) * theta.cos(),
-            p0.y + (length * 0.5) * theta.sin()
-        );
-        inner.push(p1);
-        outer.push(p2);
-    }
-    // Combine inner and outer points into a single Vec. Note we reverse the outer Vec.
-    let mut points = vec!();
-    for p1 in inner.drain(0..) {
-        points.push(p1);
-    }
-    for p2 in outer.drain(0..).rev() {
-        points.push(p2);
-    }
-    (size, points)
-}
+    /// Y coordinate of the center of the spiral
+    #[clap(default_value_t = 250.0, short = 'y', long, value_parser)]
+    origin_y: f64,
 
-fn get_average_rgb(pixels: &Vec<&Rgb<u8>>) -> Rgb<u8> {
-    let mut r = 0.0;
-    let mut g = 0.0;
-    let mut b = 0.0;
-    let mut count = 0.0;
-    for i in pixels {
-        r += i.data[0] as f64;
-        g += i.data[1] as f64;
-        b += i.data[2] as f64;
-        count += 1.0;
-    }
-    r = r / count;
-    g = g / count;
-    b = b / count;
-    return Rgb([r.round() as u8, g.round() as u8, b.round() as u8])
-}
-
-fn get_average_rgb_between_points(source: &RgbImage, p1: &Point, p2: &Point) -> Rgb<u8> {
-    let dist = p1.dist(&p2) as u32;
-    let theta = p1.angle(&p2);
-    let (width, height) = source.dimensions();
-    let mut pixels = vec!();
-    for i in 0..dist {
-        let sx = (p1.x + i as f64 * theta.cos()).round() as u32;
-        let sy = (p1.y + i as f64 * theta.sin()).round() as u32;
-        if sx > width - 1 || sy > height - 1 {
-            continue;
-        }
-        let pixel = source.get_pixel(sx, sy);
-        pixels.push(pixel);
-    };
-    get_average_rgb(&pixels)
+    /// Inverts the image
+    #[clap(default_value_t = false, short, long, value_parser)]
+    invert: bool,
 }
 
 fn main() {
-    let matches = App::new("swirlr")
-        .version("1.1")
-        .author("Will Dady <willdady@gmail.com>")
-        .about("Creates an SVG from an input image sampling along the path of an archimedian spiral")
-        .arg(
-            Arg::with_name("input")
-                .index(1)
-                .value_name("INPUT")
-                .help("Path to input image")
-                .required(true)
-                .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("color")
-                .long("color")
-                .value_name("COLOR")
-                .help("Fill color")
-                .takes_value(true)
-                .default_value("black")
-        )
-        .get_matches();
+    let cli = Cli::parse();
+    let input_path = cli.source;
+    let color = cli.color;
+    let max_radius = cli.max_radius;
+    let growth_rate = cli.growth_rate;
+    let origin_x = cli.origin_x;
+    let origin_y = cli.origin_y;
+    let invert = cli.invert;
+    let bg_color = cli.bg_color;
 
-    let input_path = matches.value_of("input").unwrap();
-    let color = matches.value_of("color").unwrap();
+    let mut source = image::open(input_path).unwrap().to_rgb8();
 
-    let mut source = image::open(input_path).unwrap().to_rgb();
-    let (size, points) = swirl(&mut source);
+    let (size, points) = Swirlr::new(&mut source)
+        .set_max_radius(max_radius)
+        .set_growth_rate(growth_rate)
+        .set_origin(origin_x, origin_y)
+        .set_invert(invert)
+        .get_points();
 
-    let mut path = svg::Path::new();
-    path.set_fill(color);
-    path.d.move_to(size * 0.5, size * 0.5);
+    let origin_point = &points[0];
 
-    let mut previous_point = &Point::new(size * 0.5, size * 0.5);
+    let mut path = svg::Path::new().set_fill(&color);
+    path.d.move_to(origin_point.x, origin_point.y);
+
+    let mut previous_point = origin_point;
 
     for p in points.iter() {
         // Only render points with at least 2 pixels distance
@@ -173,7 +82,12 @@ fn main() {
 
     path.d.close();
 
-    print!("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 {size} {size}\" width=\"{size}\" height=\"{size}\">
-        {path}
-    </svg>", size=size, path=path);
+    let mut _svg = svg::Svg::new(size as u32, size as u32);
+    if bg_color.is_some() {
+        _svg.add_child(Box::new(
+            svg::Rect::new(size as u32, size as u32).set_fill(bg_color),
+        ));
+    }
+    _svg.add_child(Box::new(path));
+    print!("{}", _svg);
 }
---
+++ src/swirlr.rs
@@ -0,0 +1,210 @@
+extern crate image;
+
+use clap::ArgEnum;
+use image::*;
+
+use crate::geometry::*;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum MaxRadius {
+    Contain,
+    Overflow,
+}
+
+pub struct Swirlr<'a> {
+    pub max_radius: MaxRadius,
+    pub origin_x: f64,
+    pub origin_y: f64,
+    pub source: &'a mut RgbImage,
+    pub output_size: f64,
+    pub growth_rate: f64,
+    pub invert: bool,
+}
+
+impl<'a> Swirlr<'a> {
+    pub fn new(source: &'a mut RgbImage) -> Swirlr {
+        Swirlr {
+            max_radius: MaxRadius::Overflow,
+            source,
+            output_size: 500.0,
+            origin_x: 250.0,
+            origin_y: 250.0,
+            growth_rate: 1.2,
+            invert: false,
+        }
+    }
+
+    pub fn set_max_radius(mut self, max_radius: MaxRadius) -> Swirlr<'a> {
+        self.max_radius = max_radius;
+        self
+    }
+
+    pub fn set_origin(mut self, x: f64, y: f64) -> Swirlr<'a> {
+        self.origin_x = x;
+        self.origin_y = y;
+        self
+    }
+
+    pub fn set_growth_rate(mut self, growth_rate: f64) -> Swirlr<'a> {
+        self.growth_rate = growth_rate;
+        self
+    }
+
+    pub fn set_invert(mut self, invert: bool) -> Swirlr<'a> {
+        self.invert = invert;
+        self
+    }
+
+    pub fn get_points(&mut self) -> (f64, Vec<Point>) {
+        // Output dimension
+        let size = 500.0;
+        // Centre-crop image to a square and resize to `size`
+        let mut im: RgbImage;
+        let (width, height) = &self.source.dimensions();
+        im = if width == height {
+            imageops::crop(self.source, 0, 0, *width, *height).to_image()
+        } else if width < height {
+            imageops::crop(
+                self.source,
+                0,
+                ((*height as f64 - *width as f64) * 0.5).floor() as u32,
+                *width,
+                *width,
+            )
+            .to_image()
+        } else {
+            imageops::crop(
+                self.source,
+                ((*width as f64 - *height as f64) * 0.5).floor() as u32,
+                0,
+                *height,
+                *height,
+            )
+            .to_image()
+        };
+        im = imageops::resize(&im, size as u32, size as u32, imageops::FilterType::Nearest);
+
+        let max_radius: f64 = match self.max_radius {
+            MaxRadius::Contain => (size * 0.5) - 5.0,
+            MaxRadius::Overflow => {
+                let tl = Point::new(0.0, 0.0);
+                let tr = Point::new(self.output_size, 0.0);
+                let br = Point::new(self.output_size, self.output_size);
+                let bl = Point::new(0.0, self.output_size);
+                let origin: Point = (self.origin_x, self.origin_y).into();
+
+                [
+                    origin.dist(&tl),
+                    origin.dist(&tr),
+                    origin.dist(&br),
+                    origin.dist(&bl),
+                ]
+                .iter()
+                .fold(f64::NEG_INFINITY, |prev, curr| prev.max(*curr))
+            }
+        };
+
+        let mut r;
+
+        let mut theta: f64 = 0.0;
+
+        let a = 0.0; // The starting radius
+        let b = self.growth_rate; // The growth rate of the spiral through each iteration of the loop
+        let sample_length = 7.0; // Controls line thickness
+
+        let mut inner = vec![];
+        let mut outer = vec![];
+
+        // while theta < max_angle {
+        loop {
+            theta += 0.003;
+            r = a + b * theta;
+            if r >= max_radius {
+                break;
+            }
+            // The current point on the spiral
+            let p0 = Point::new(
+                self.origin_x + r * theta.cos(),
+                self.origin_y + r * theta.sin(),
+            );
+            // We generate two points centered around p0 in the direction of theta,
+            // one towards the center of the spiral, the other away from.
+            let p1 = Point::new(
+                p0.x - (sample_length * 0.5) * theta.cos(),
+                p0.y - (sample_length * 0.5) * theta.sin(),
+            );
+            let p2 = Point::new(
+                p0.x + (sample_length * 0.5) * theta.cos(),
+                p0.y + (sample_length * 0.5) * theta.sin(),
+            );
+            // Get the average rgb between our two points
+            let average_rgb = get_average_rgb_between_points(&im, &p1, &p2);
+            let luma = average_rgb.to_luma();
+            // Convert average rgb into luma and then normalise the luma to a value
+            // between 0 and sample_length
+            let mut length = ((255.0 - luma[0] as f64) / 255.0) * sample_length;
+
+            if self.invert {
+                length = sample_length - length;
+            }
+
+            // Make sure length is not less than 1, purely for asthetic reasons
+            if length < 1.0 {
+                length = 1.0;
+            }
+            let p1 = Point::new(
+                p0.x - (length * 0.5) * theta.cos(),
+                p0.y - (length * 0.5) * theta.sin(),
+            );
+            let p2 = Point::new(
+                p0.x + (length * 0.5) * theta.cos(),
+                p0.y + (length * 0.5) * theta.sin(),
+            );
+            inner.push(p1);
+            outer.push(p2);
+        }
+        // Combine inner and outer points into a single Vec. Note we reverse the outer Vec.
+        let mut points = vec![];
+        for p1 in inner.drain(0..) {
+            points.push(p1);
+        }
+        for p2 in outer.drain(0..).rev() {
+            points.push(p2);
+        }
+        (size, points)
+    }
+}
+
+fn get_average_rgb(pixels: &Vec<&Rgb<u8>>) -> Rgb<u8> {
+    let mut r = 0.0;
+    let mut g = 0.0;
+    let mut b = 0.0;
+    let mut count = 0.0;
+    for i in pixels {
+        r += i[0] as f64;
+        g += i[1] as f64;
+        b += i[2] as f64;
+        count += 1.0;
+    }
+    r = r / count;
+    g = g / count;
+    b = b / count;
+    return Rgb([r.round() as u8, g.round() as u8, b.round() as u8]);
+}
+
+fn get_average_rgb_between_points(source: &RgbImage, p1: &Point, p2: &Point) -> Rgb<u8> {
+    let dist = p1.dist(&p2) as u32;
+    let theta = p1.angle(&p2);
+    let (width, height) = source.dimensions();
+    let mut pixels = vec![];
+    for i in 0..dist {
+        let sx = (p1.x + i as f64 * theta.cos()).round() as u32;
+        let sy = (p1.y + i as f64 * theta.sin()).round() as u32;
+        if sx > width - 1 || sy > height - 1 {
+            continue;
+        }
+        let pixel = source.get_pixel(sx, sy);
+        pixels.push(pixel);
+    }
+    get_average_rgb(&pixels)
+}
